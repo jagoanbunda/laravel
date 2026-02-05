de9d8e94c41c1745a31558d8c65d9f05a02418b7
reky <rekyardhana029@gmail.com>
2026-02-05 19:27:19 +0700

    feat(nutrition): add Indonesian RDA config for dashboard calculations

---
+++ config/nutrition.php
@@ -0,0 +1,138 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Indonesian RDA (AKG 2019) Nutrition Targets
+    |--------------------------------------------------------------------------
+    |
+    | This file contains the recommended daily allowance (RDA) values for
+    | macronutrients based on Indonesian AKG 2019 (Angka Kecukupan Gizi).
+    | Values are organized by age group for child nutrition monitoring.
+    |
+    */
+
+    'source' => 'Indonesian AKG 2019',
+
+    'age_groups' => [
+
+        '0-6' => [
+            'label' => '0-6 months',
+            'calories' => [
+                'value' => 550,
+                'unit' => 'kcal',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'protein' => [
+                'value' => 12,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'carbs' => [
+                'value' => 58,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'fat' => [
+                'value' => 34,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'fiber' => [
+                'value' => 0,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+        ],
+
+        '6-12' => [
+            'label' => '6-12 months',
+            'calories' => [
+                'value' => 800,
+                'unit' => 'kcal',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'protein' => [
+                'value' => 18,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'carbs' => [
+                'value' => 82,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'fat' => [
+                'value' => 36,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'fiber' => [
+                'value' => 10,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+        ],
+
+        '1-3' => [
+            'label' => '1-3 years',
+            'calories' => [
+                'value' => 1350,
+                'unit' => 'kcal',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'protein' => [
+                'value' => 20,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'carbs' => [
+                'value' => 155,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'fat' => [
+                'value' => 45,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'fiber' => [
+                'value' => 16,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+        ],
+
+        '4-6' => [
+            'label' => '4-6 years',
+            'calories' => [
+                'value' => 1400,
+                'unit' => 'kcal',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'protein' => [
+                'value' => 25,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'carbs' => [
+                'value' => 175,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'fat' => [
+                'value' => 50,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+            'fiber' => [
+                'value' => 22,
+                'unit' => 'g',
+                'source' => 'Indonesian AKG 2019',
+            ],
+        ],
+
+    ],
+
+];
